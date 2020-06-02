f47734afd394b12a9650fa7070d7c5134f9f29fd
Most <mcadenhe@gmail.com>
2020-06-02 15:53:42 -0500

    Style shop by brand section

--- test/gearflow_demo_web/live/page_live_test.exs
+++ test/gearflow_demo_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule GearflowDemoWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "Shop by Brand"
+    assert render(page_live) =~ "Shop by Brand"
   end
 end
