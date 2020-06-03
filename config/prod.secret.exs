e82237a3baaec55b61ff25ce1ead1f979d1eec38
Most <mcadenhe@gmail.com>
2020-06-02 19:07:30 -0500

    Use production config for Heroku and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :gearflow_demo, GearflowDemo.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
